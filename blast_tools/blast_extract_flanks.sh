7a05fdebe430e59cb8e63eecf2f8a0f2b5d636cc
Jennifer Molinet <jennifer.molinet@usach.cl>
2025-08-29 10:42:35 -0400

    Create blast_extract_flanks.sh

---
+++ blast_tools/blast_extract_flanks.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Query (gene of interest, nucleotide sequence)
+QUERY="gen_interes.fasta"
+
+# List of genomes (FASTA files must exist: SA.fasta, SCH.fasta, SJ.fasta, etc.)
+GENOMAS=("SA" "SCH" "SJ")
+
+# Flanking size (bp)
+FLANK=1000
+
+# Global results file with header
+echo -e "genome\tqseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqstart\tqend\tsstart\tsend\tevalue\tbitscore" > resultados_todos.txt
+
+# Loop over genomes
+for G in "${GENOMAS[@]}"; do
+    GENOMA="${G}.fasta"
+    DB="${G}_db"
+
+    echo "Processing $GENOMA ..."
+
+    # Create BLAST database
+    makeblastdb -in $GENOMA -dbtype nucl -out $DB
+
+    # Run BLAST and keep the best hit
+    BEST_HIT=$(blastn -query $QUERY -db $DB \
+        -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore" \
+        | sort -k12,12nr | head -n 1)
+
+    # Save to global results file
+    echo -e "${G}\t${BEST_HIT}" >> resultados_todos.txt
+
+    # Extract fields
+    SSEQID=$(echo $BEST_HIT | awk '{print $2}')
+    SSTART=$(echo $BEST_HIT | awk '{print $9}')
+    SEND=$(echo $BEST_HIT | awk '{print $10}')
+
+    # Ensure start < end
+    if [ $SSTART -le $SEND ]; then
+        START=$SSTART
+        END=$SEND
+        REVCOMP=0
+    else
+        START=$SEND
+        END=$SSTART
+        REVCOMP=1
+    fi
+
+    # Add flanking regions
+    FLANK_START=$(( START - FLANK ))
+    if [ $FLANK_START -lt 1 ]; then
+        FLANK_START=1
+    fi
+    FLANK_END=$(( END + FLANK ))
+
+    REGION="${SSEQID}:${START}-${END}"
+    REGION_FLANK="${SSEQID}:${FLANK_START}-${FLANK_END}"
+
+    # Extract gene sequence
+    samtools faidx $GENOMA $REGION > ${G}_hit_tmp.fasta
+    # Extract sequence with flanks
+    samtools faidx $GENOMA $REGION_FLANK > ${G}_flank_tmp.fasta
+
+    # Reverse-complement if needed
+    if [ $REVCOMP -eq 1 ]; then
+        seqkit seq -r -p ${G}_hit_tmp.fasta > ${G}_hit.fasta
+        seqkit seq -r -p ${G}_flank_tmp.fasta > ${G}_flank.fasta
+        rm ${G}_hit_tmp.fasta ${G}_flank_tmp.fasta
+    else
+        mv ${G}_hit_tmp.fasta ${G}_hit.fasta
+        mv ${G}_flank_tmp.fasta ${G}_flank.fasta
+    fi
+
+    echo "Best hit sequence saved in: ${G}_hit.fasta"
+    echo "Best hit with ±${FLANK} bp flanks saved in: ${G}_flank.fasta"
+done
